eca0026e75ee2a6733163e0d9e5f4e420cb7c57c
Zein Reyes <zein.reyes2005@gmail.com>
2024-10-08 11:14:45 +0800

    first commit

---
+++ jsonData.js
@@ -0,0 +1,26 @@
+const jsonData = {
+    "Catalog": {
+        "products": [
+            {
+                "ProductID": "101",
+                "Name": "Smartphone",
+                "Category": "Electronics",
+                "Price": 299.99,
+                "Stock": 25,
+                "Description": "A high-end smartphone with excellent features.",
+                "ImageURL": "https://www.apple.com/newsroom/images/2024/09/apple-debuts-iphone-16-pro-and-iphone-16-pro-max/article/Apple-iPhone-16-Pro-hero-geo-240909_inline.jpg.large_2x.jpg"
+            },
+            {
+                "ProductID": "102",
+                "Name": "Laptop",
+                "Category": "Electronics",
+                "Price": 799.99,
+                "Stock": 15,
+                "Description": "A powerful laptop for gaming.",
+                "ImageURL": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsvhN_ggL0HY-2NepVUH2sPf1kX9akdHCLIg&s"
+            }
+        ]
+    }
+};
+
+module.exports = jsonData;
